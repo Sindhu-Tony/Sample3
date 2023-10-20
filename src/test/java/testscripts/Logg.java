a57cb2a505a7caf6ce64c6a6cc4b42999c5a45ad
Sindhu-Tony <sindhuhere@gmail.com>
2023-10-20 12:32:40 +0000

    first cc

---
+++ src/test/java/testscripts/Logg.java
@@ -0,0 +1,22 @@
+package testscripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Logg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+				WebDriver driver=new ChromeDriver();
+				driver.get("http://watir.com/examples/shadow_dom.html");
+				WebElement shadowHost=driver.findElement(By.cssSelector("#shadow_host"));
+			SearchContext shadowRoot = shadowHost.getShadowRoot();
+			WebElement shadowContent = shadowRoot.findElement(By.cssSelector("#shadow_content"));
+			System.out.println(shadowContent.getText());
+	}
+
+}
